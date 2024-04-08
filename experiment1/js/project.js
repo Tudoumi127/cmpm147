4aa712c78766baf8c0f27bd7055f5dfde4a94c2d
Tudoumi127 <123220803+Tudoumi127@users.noreply.github.com>
2024-04-07 20:13:01 -0700

    Trying to copy stuff from Glitch attempt 1

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -23,12 +23,59 @@ class MyProjectClass {
 }
 
 function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
-
-  // call a method on the instance
-  myInstance.myMethod();
+  const fillers = {
+    character: ["Katsuhiko", "Jamie", "Mingyou", "Victoria", "Daiyu", "Eun-ji", "Kaile", "Ana", "Ariel", "Lin", "Viva", "Chris"],
+    pet: ["YaYa", "YueYue", "PomPom", "Paimon", "Morgana", "Diamond Dogs"],
+    animal: ["diamond dogs", "League of Legends player", "Valorant player", "incel", "Genshin Impact player", "tiger", "cat", "dog", "pig"],
+    location: ["The Divining Sanctum", "The Mall", "Katsuhiko's House", "The Pool", "Hell", "Heaven", "The Good Place", "The Bad Place", "Chris Evans' No-Longer-Existent twitter"],
+    celebrity: ["Scarlett Johansson", "Chris Hemsworth", "Tom Holland", "Jojo Siwa", "Pewdiepie", "Zach Galifianakis", "Joseph Stalin", "Mao Zedong", "Kim Jong-un"],
+    adj: ["gruesome", "funny", "hilarious", "crazy", "insane", "toe-tickling", "exciting", "hahaheehee"],
+    verb: ["seducing", "slapping", "stealing", "kissing", "killing", "licking", "kicking", "choking", "snatching"],
+    fictional: ["anime", "cartoon", "fictional", "fake", "isekai", "test"],
+    item: ["knife", "gun", "teddy bear", "mace", "taser", "hammer", "bird", "bullet proof vest", "chain mail", "fist", "potato"],
+    sendoff: ["make haste", "get out of here", "hurry off", "screw off", "off you go"],
+    belonging: ["pants", "wig", "cat", "shears", "shoes", "nail clippers", "ear wax", "pain medication"]
+    
+  };
+  
+  const template = `$character help, $character needs you!
+  
+  They went to $location to meet $character and got mauled by a $animal that kind of looked like $pet mixed with $celebrity. It was kinda $adj to be honest. Its okay though because they'll heal fully since this is a $fictional world! But they need your help getting to $location.
+  
+  Hurry though because if $character gets to them before you they'll never let you hear the end of it. They're still mad at you for $verb and $verb their ex-partner $character.
+  
+  Here take this $item with you, might help you fight them off if that happens. Anyway, run now! $sendoff ! If you run into $character on the way there too, tell them I still have their $belonging.
+  `;
+  
+  
+  // STUDENTS: You don't need to edit code below this line.
+  
+  const slotPattern = /\$(\w+)/;
+  
+  function replacer(match, name) {
+    let options = fillers[name];
+    if (options) {
+      return options[Math.floor(Math.random() * options.length)];
+    } else {
+      return `<UNKNOWN:${name}>`;
+    }
+  }
+  
+  function generate() {
+    let story = template;
+    while (story.match(slotPattern)) {
+      story = story.replace(slotPattern, replacer);
+    }
+  
+    /* global box */
+    $('#box').text(story);
+  }
+  
+  /* global clicker */
+  $("#clicker").click(generate);
+  
+  generate();
 }
 
 // let's get this party started - uncomment me
-//main();
+main();
