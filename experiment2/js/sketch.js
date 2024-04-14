44d06cd3b743f086dbb28b7c63aeaab4b3adfa80
Tudoumi127 <123220803+Tudoumi127@users.noreply.github.com>
2024-04-13 19:55:23 -0700

    Finished i think?

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -7,24 +7,6 @@
 
 // Constants - User-servicable parts
 // In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
-// Globals
-let myInstance;
-let canvasContainer;
-var centerHorz, centerVert;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
 
 function resizeScreen() {
   centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
@@ -42,38 +24,113 @@ function setup() {
   canvas.parent("canvas-container");
   // resize canvas is the page is resized
 
-  // create an instance of the class
-  myInstance = new MyClass("VALUE1", "VALUE2");
-
   $(window).resize(function() {
     resizeScreen();
   });
   resizeScreen();
+
+  createButton("reimagine").mousePressed(() => {seed++; count = 0;});
+  canvas.mousePressed(() => {flowerFlag = true; clickedFlowers.push([mouseX, mouseY]);});
 }
 
-// draw() function is called repeatedly, it's the main animation loop
+/* exported setup, draw */
+let seed = 2049;
+
+const skyColor = "#2a4180";
+const grassColor = "#98a54b";
+const mountainColor = "#e4e3e6";
+const mountainShadow = "bfc2d0";
+const flower1Color = [244, 188, 237];
+const flower2Color = [168, 52, 137];
+const flower3Color = [102, 204, 255];
+let clickedFlowers = []; // save mouseX, mouseY values
+let flowerFlag = false;
+
+
 function draw() {
-  background(220);    
-  // call a method on the instance
-  myInstance.myMethod();
-
-  // Set up rotation for the rectangle
-  push(); // Save the current drawing context
-  translate(centerHorz, centerVert); // Move the origin to the rectangle's center
-  rotate(frameCount / 100.0); // Rotate by frameCount to animate the rotation
-  fill(234, 31, 81);
+  randomSeed(seed);
+  background(100);
+  
   noStroke();
-  rect(-125, -125, 250, 250); // Draw the rectangle centered on the new origin
-  pop(); // Restore the original drawing context
-
-  // The text is not affected by the translate and rotate
-  fill(255);
-  textStyle(BOLD);
-  textSize(140);
-  text("p5*", centerHorz - 105, centerVert + 40);
+  
+  //sky
+  fill(skyColor);
+  rect(0, 0, width, height / 2.5);
+  
+  
+  //grass
+  fill(grassColor);
+  rect(0, height / 2.5, width, height / 1.5);
+  
+  //mountain base
+  fill(mountainColor);
+  beginShape();
+  let offset = 15;
+  vertex(0, height / 2 - offset);
+  const steps = 10;
+  for (let i = 0; i < steps + 1; i++) {
+    let x = (width * i) / steps;
+    let y =
+      height / 2 - (random() * random() * random() * height) - height / 30;
+    vertex(x, y - offset);
+  }
+  vertex(width, height / 2);
+  endShape(CLOSE);
+  
+  fill(mountainShadow);
+  beginShape();
+  let offset2 = 15;
+  vertex(0, height / 2 - offset);
+  const steps2 = 10;
+  for (let i = 0; i < steps + 1; i++) {
+    let x = (width * i) / steps;
+    let y =
+      height / 2 - (random() * random() * random() * height) / 2 - height / 30;
+    vertex(x, y - offset2);
+  }
+  vertex(width, height / 2);
+  endShape(CLOSE);
+  
+  if (flowerFlag) {
+    fill(...flower1Color, count);
+    const trees = 50*random();
+    const scrub = mouseX/width;
+    for (let i = 0; i < trees; i++) {
+      let z = random(0, 1);
+      let x = width * ((random(0, 10) + (scrub/50 + millis() / 500000.0) / z) % 1);
+      // let x = width * random(0, 1);
+      //console.log(x);
+      // let s = width / 50 / z;
+      let s = z * 20;
+      let y = height / 2 + height / 20 / z;
+      ellipse(x, y, s, s);
+    }
+    fill(...flower2Color, count);
+    //const trees = 20*random();
+    //const scrub = mouseX/width;
+    for (let i = 0; i < trees; i++) {
+      let z = random(0, 1);
+      let x = width * ((random(0, 10) + (scrub/50 + millis() / 500000.0) / z) % 1);
+      // let x = width * random(0, 1);
+      //console.log(x);
+      // let s = width / 50 / z;
+      let s = z * 20;
+      let y = height / 2 + height / 20 / z;
+      ellipse(x, y, 20-s, 20-s);
+    }
+    
+       
+    fill(...flower3Color, count);
+    for (let i = 0; i < clickedFlowers.length; i++){
+      ellipse(...clickedFlowers[i], random()* 30);
+    }
+    
+    count -= 0.8;
+    if(count <= 0){
+      count = 255;
+      flowerFlag = false;
+      clickedFlowers = [];
+    }
+  }
 }
-
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
-}
+let count = 255;
